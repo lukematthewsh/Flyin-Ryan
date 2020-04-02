c8dc9c7902b39bf6fe27f063548dd304b5c1a877
ZenArcade17 <59713429+ZenArcade17@users.noreply.github.com>
2020-04-02 17:05:01 -0400

    firebaseApp.js set up

---
+++ firebaseApp.js
@@ -0,0 +1,24 @@
+import firebase from 'firebase/app'
+import 'firebase/database'
+import 'firebase/auth'
+
+const credentials = {
+    apiKey: "AIzaSyAIivJHZIf8S0tS4vASoZXGeLaPN2pJ-58",
+    authDomain: "flyin-ryan-database.firebaseapp.com",
+    databaseURL: "https://flyin-ryan-database.firebaseio.com",
+    projectId: "flyin-ryan-database",
+    storageBucket: "flyin-ryan-database.appspot.com",
+    messagingSenderId: "58276712825",
+    appId: "1:58276712825:web:22d8e1b5a8bbc4fa371fa8",
+    measurementId: "G-MZ83BH5QMY"
+  }
+
+
+const firebaseApp = !firebase.apps.length ? firebase.initializeApp(credentials) : firebase.app()
+
+
+const database = firebaseApp.database()
+const googleProvider = new firebase.auth.GoogleAuthProvider()
+const auth = firebase.auth
+
+export { firebaseApp, database, auth, googleProvider }
