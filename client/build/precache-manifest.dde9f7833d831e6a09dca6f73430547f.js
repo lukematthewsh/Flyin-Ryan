7c62da259faf6dc62ab2568456f9dc4e4462d0d8
ZenArcade17 <59713429+ZenArcade17@users.noreply.github.com>
2020-04-02 14:14:40 -0400

    basic server

---
+++ client/build/precache-manifest.dde9f7833d831e6a09dca6f73430547f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7aa2301732392b3cd9190b038d8ddbae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b9ce6902591d4b3d25f",
+    "url": "/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "79e22a8077b5e84861f5",
+    "url": "/static/js/2.e58021ce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e58021ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b9ce6902591d4b3d25f",
+    "url": "/static/js/main.f30c0125.chunk.js"
+  },
+  {
+    "revision": "1e1bf242d2a88b61b869",
+    "url": "/static/js/runtime-main.0d7df7ae.js"
+  }
+]);
---
+++ server.js
@@ -0,0 +1,22 @@
+//defining variables for express, path
+const express = require('express')
+const path = require('path')
+
+const app = express()
+const port = process.env.PORT || 5000;
+
+//defining the path to launch the site from
+
+app.use(express.static(path.resolve('./client/public')))
+
+//defining the file that will be launched when site is visited
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve('./client/public/index.html'))
+})
+
+//telling it to listen for this port
+
+app.listen(port, () => {
+    console.log('Listening on port: ', port)
+})
