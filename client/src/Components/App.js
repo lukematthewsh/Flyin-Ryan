5481a20d2783944c3ba3e8f8dcbc111cd31ceedf
Luke Matthews <lukematthewsh@gmail.com>
2020-04-03 13:06:19 -0400

    home page commit

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import '../Css/App.css';
 import Landing from "./Landing.js"
-
+import Header from './Header'
 
 function App() {
   return (
     <div id="app">
+      <Header/>
       <Landing/>
       <div id='header'>
         <div id='dropdown'>
---
+++ client/src/Components/MenuButton.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import '../Css/MenuButton.css';
+ 
+class MenuButton extends Component {
+  render() {
+    return (
+      <button id="roundButton"
+              onMouseDown={this.props.handleMouseDown}>Menu</button>
+    );
+  }
+}
+ 
+export default MenuButton
