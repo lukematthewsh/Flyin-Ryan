0c5fdb8415d7b71199d2b4053810e6376e59d862
DevonSmith91 <DevonSmith91@gmail.com>
2020-04-06 17:38:53 -0400

    fixed zindex things

--- client/src/Components/Questions.js
+++ client/src/Components/Questions.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import '../Css/Questions.css'
 
 class Questions extends React.Component {
     constructor(props) {
@@ -7,7 +8,7 @@ class Questions extends React.Component {
 
     render() {
         return(
-            <div>
+            <div id='question-container'>
                 <h1>Hello!</h1>
             </div>
         )
