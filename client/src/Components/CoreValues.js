ca67ea4c34ed7d0b0f0388e25e9e06c1acba8286
Luke Matthews <lukematthewsh@gmail.com>
2020-04-14 09:25:53 -0400

    progress bar

--- client/src/Components/CoreValues.js
+++ client/src/Components/CoreValues.js
@@ -9,32 +9,39 @@ class CoreValues extends React.Component {
         super(props)
         this.state = {
             user: this.props.user,
-            userData: null,
+            userData: [],
 
         }
 
     }
-    getCoreValues = (uid) => {
-        database.ref('users/' + this.state.user.uid + '/11/answer').once("value", snap => {
 
+
+    getCoreValues = (uid) => {
+
+        database.ref('users/' + this.state.user.uid + '/Key Core Values').once("value", snapshot => { 
             this.setState({
-                userData: snap.val()
+                userData: snapshot.val()
             })
+            
+           
         })
     }
+
+
+
     componentDidMount() {
         this.getCoreValues();
-     }
-    
-     componentWillUnmount() { 
-       window.removeEventListener('load', this.getCoreValues)  
-     }
-    
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('load', this.getCoreValues)
+    }
+
     render() {
         return (
             <div id='core-values'>
                 <h1>Your Core Values</h1>
-                <div id = "corevalues-content">{this.state.userData}</div>
+                <div id="corevalues-content">{this.state.userData}</div>
                 <br></br>
                 <h3>This where the user's core values will be displayed. They will also be able to update/edit them here.</h3>
             </div>
