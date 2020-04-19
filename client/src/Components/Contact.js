3ffa5389e71acdd76e8e5a313fdf918993589e27
Ilya Sadigurski <isadigurski@gmail.com>
2020-04-19 17:38:20 -0400

    Added social media svg files to contact page

--- client/src/Components/Contact.js
+++ client/src/Components/Contact.js
@@ -2,9 +2,13 @@ import React from 'react';
 import '../Css/Contact.css';
 import Header from './Header';
 import Mailer from './Mailer.js';
-import TwitterIcon from '../Icons/twitter.png';
-import FaceIcon from '../Icons/facebook.png';
-import InstaIcon from '../Icons/instagram.png';
+
+
+import InstaIcon from '../Icons/instagram.svg';
+import FaceIcon from '../Icons/facebook.svg';
+import YouTubeIcon from '../Icons/youtube.svg';
+import FlikerIcon from '../Icons/fliker.svg';
+import TwitterIcon from '../Icons/twitter.svg';
 
 class Contact extends React.Component {
 
@@ -38,6 +42,14 @@ class Contact extends React.Component {
                     <div className='icon'>
                         <a href="https://www.facebook.com/flyinryanhawks/">
                             <img className='logo-blip' src={FaceIcon} alt="facebook" /></a>
+                    </div>
+                    <div className='icon'>
+                        <a href="https://www.youtube.com/user/FlyinRyanHawks/">
+                            <img className='logo-blip' src={YouTubeIcon} alt="youtube" /></a>
+                    </div>
+                    <div className='icon'>
+                        <a href="https://www.flickr.com/photos/flyinryanhawks/">
+                            <img className='logo-blip' src={FlikerIcon} alt="fliker" /></a>
                     </div>
                     <div className='icon'>
                         <a href="https://twitter.com/flyinryanhawks/">
