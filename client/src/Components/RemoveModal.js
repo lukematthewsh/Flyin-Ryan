73a41c2e331feb6a247acfa8a7aa8fbe40d0c7b0
DevonSmith91 <DevonSmith91@gmail.com>
2020-04-20 19:48:12 -0400

    fixed a bug with the delete function

--- client/src/Components/RemoveModal.js
+++ client/src/Components/RemoveModal.js
@@ -3,31 +3,35 @@ import X from "../images/x.png"
 import "../Css/ShareModal.css"
 import { database } from '../firebaseApp.js'
 
-class RemoveModal extends React.Component{
-    constructor(props){
+class RemoveModal extends React.Component {
+    constructor(props) {
         super(props)
     }
 
 
 
-    render(){
+    render() {
         if (!this.props.show) {
             return null;
         }
-        return(
-            <div id = "share-modal">
+        return (
+            <div id="share-modal">
                 <h2>Remove Value</h2>
                 <br></br>
-                
-                <div onClick={this.props.closeRemModal}><img id='close-button' src={X} style={{ maxWidth: "40px" }}></img></div>
-                <div id = 'preshare'>
-               <div>Hey, <span id = "author-post">{this.props.author}</span>, click "Delete Value" to delete the following value: <div id ="post-content">{this.props.content}</div></div>
-               
-               </div>
-              
-               <div id ="share-to-feed" onClick= {this.props.delete}>Delete Value</div>
-                
-
+                <div onClick={this.props.closeRemModal}>
+                    <img id='close-button' src={X} style={{ maxWidth: "40px" }}></img>
+                </div>
+                <div id='preshare'>
+                    <div>Hey, 
+                        <span id="author-post">
+                            {this.props.author}
+                        </span>, click "Delete Value" to delete the following value:
+                        <div id="post-content">
+                            {this.props.content}
+                        </div>
+                    </div>
+                </div>
+                <div id="share-to-feed" onClick={this.props.delete}>Delete Value</div>
             </div>
         )
     }
