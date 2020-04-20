a933e3f89605df8907c4bbe0dcd6a3ef577e1c7a
ZenArcade17 <59713429+ZenArcade17@users.noreply.github.com>
2020-04-20 00:08:52 -0400

    users can now add values from dashboard

---
+++ client/src/Components/AddModal.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import X from "../images/x.png"
+import "../Css/ShareModal.css"
+import { database } from '../firebaseApp.js'
+
+
+class AddModal extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+    
+    render() {
+        if (!this.props.show) {
+            return null;
+        }
+        return (
+            <div id="share-modal">
+                <h2>Add a Core Value</h2>
+                <br></br>
+                <br></br>
+                <div onClick={this.props.closeAddModal}><img id='close-button' src={X} style={{ maxWidth: "40px" }}></img></div>
+                    <textarea id='add-value-text' placeholder="Enter a New Core Value Here!" ></textarea>
+
+                    <div id="share-to-feed" onClick={this.props.addValue}>Add Value</div>
+                
+
+            </div>
+        )
+    }
+}
+
+export default AddModal
--- client/src/Components/CoreValues.js
+++ client/src/Components/CoreValues.js
@@ -7,6 +7,8 @@ import { Link } from 'react-router-dom'
 import shareFacebook from 'share-facebook'
 import facebook from '../images/facebook.png'
 import ShareModal from './ShareModal.js'
+import AddModal from './AddModal'
+import PlusSign from '../images/Plus Sign.jpg'
 
 
 class CoreValues extends React.Component {
@@ -14,7 +16,8 @@ class CoreValues extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            show:  false,
+            show: false,
+            showAdd: false,
             user: this.props.user,
             content: [],
             userData: [],
@@ -34,8 +37,16 @@ class CoreValues extends React.Component {
         this.setState({
             userData: answers
         })
+        console.log(this.state.userData)
     }
-    openShareModal = (event) =>{
+
+    openAddModal = (event) => {
+        this.setState({
+            showAdd: !this.state.showAdd
+        })
+    }
+
+    openShareModal = (event) => {
         this.setState({
             show: !this.state.show,
             content: (event.target.parentNode.parentNode.textContent)
@@ -43,12 +54,30 @@ class CoreValues extends React.Component {
         )
 
     }
-    closeShareModal = (e) =>{
+
+    closeAddModal = (event) => {
+        this.setState({
+            showAdd: !this.state.showAdd
+        })
+    }
+
+    closeShareModal = (e) => {
         this.setState({
             show: !this.state.show
         }
         )
 
+    }
+
+    addValue = async () => {
+        let newValue = document.getElementById('add-value-text').value
+        this.state.userData.push(newValue)
+        let update = {}
+        update[`/users/${this.state.user.uid}/Key Core Values`] = this.state.userData
+        await database.ref().update(update)
+        this.setState({
+            showAdd: !this.state.showAdd
+        })
     }
 
 
@@ -60,50 +89,52 @@ class CoreValues extends React.Component {
             href: 'http://localhost:3000/dashboard',
             redirect_uri: 'https://www.google.com',
             app_id: '2367781363516455'
-          })
-          console.log(test)
+        })
+        console.log(test)
     }
 
     render() {
 
-        if (this.state.userData === null){
-            return(
-                <div id = "core-values">
-                <h1>Your Core Values</h1>
-                <div id = "rasta-border-core"></div>
-                <h4>It appears you havent filled out you core values yet! Click below to start!</h4>
-                <Link to = {"/questions"} style={{ textDecoration: 'none' }}><div id="get-started" >Get Started</div></Link>
+        if (this.state.userData === null) {
+            return (
+                <div id="core-values">
+                    <h1>Your Core Values</h1>
+                    <div id="rasta-border-core"></div>
+                    <h4>It appears you havent filled out you core values yet! Click below to start!</h4>
+                    <Link to={"/questions"} style={{ textDecoration: 'none' }}><div id="get-started" >Get Started</div></Link>
                 </div>
             )
         }
-        else{ 
-        return (
-            
-            this.state.userData !== null ?
-            <div id='core-values'>
-                <ShareModal  show={this.state.show}  closeShareModal={this.closeShareModal} content ={this.state.content} author ={this.state.author}/>
-                <h1>Your Core Values</h1>
-                <div id = "rasta-border-core"></div>
-                <h4>Here you can view your Core Values! You will also be able to update/edit them here.</h4>
-                <br></br>
-                <div id="corevalues-content">
-                    <ul id="core-values-list">
-                        {this.state.userData.map(item => (
-                            <div id="button-core-container" key={item}>
-                                <h5 id = "value">{item}</h5>
-                                <div id="edit" onClick={this.edit}> <img src={editIco} style={{ maxWidth: "15px" }} /></div>
-                                <div onClick={this.openShareModal}><img id="share" src={ShareIco} style={{ maxWidth: "15px" }} /></div>
-                                <div onClick={this.facebook}><img id="facebook-img" src={facebook} /></div>
-                            </div>
-                        ))}
-                    </ul>
-                </div>
-                <br></br>
+        else {
+            return (
 
-            </div> : <Link to="/questions"><h1 style={{marginTop: 200}}>Take the Core Values Challenge!</h1></Link>
-        )
+                this.state.userData !== null ?
+                    <div id='core-values'>
+                        <ShareModal show={this.state.show} closeShareModal={this.closeShareModal} content={this.state.content} author={this.state.author} />
+                        <AddModal addValue={this.addValue} user={this.state.user} userData={this.state.userData} show={this.state.showAdd} closeAddModal={this.closeAddModal} author={this.state.author} />
+                        <h1>Your Core Values</h1>
+                        <div id="rasta-border-core"></div>
+                        <h4>Here you can view your Core Values! You will also be able to update/edit them here.</h4>
+                        <br></br>
+                        <div id="corevalues-content">
+                            <ul id="core-values-list">
+                                {this.state.userData.map(item => (
+                                    <div id="button-core-container" key={item}>
+                                        <h5 id="value">{item}</h5>
+                                        <div id="edit" onClick={this.edit}> <img src={editIco} style={{ maxWidth: "15px" }} /></div>
+                                        <div onClick={this.openShareModal}><img id="share" src={ShareIco} style={{ maxWidth: "15px" }} /></div>
+                                        <div onClick={this.facebook}><img id="facebook-img" src={facebook} /></div>
+                                    </div>
+                                ))}
+                            </ul>
+                        </div>
+                        <br></br>
+                        <div id='plus'><img onClick={this.openAddModal} id='plus-sign' src={PlusSign} alt='plus-sign' /></div>
+
+                    </div> : <Link to="/questions"><h1 style={{ marginTop: 200 }}>Take the Core Values Challenge!</h1></Link>
+            )
+        }
     }
-}
 }
 
 export default CoreValues
