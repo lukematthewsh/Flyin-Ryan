7ebaca971323af42300dced86557cd8a2ba17e6c
Luke Matthews <lukematthewsh@gmail.com>
2020-04-27 12:53:15 -0400

    stuff

--- client/src/Components/CoreValues.js
+++ client/src/Components/CoreValues.js
@@ -200,7 +200,7 @@ class CoreValues extends React.Component {
                                 <div id="button-core-container" key={item}>
                                     <h5 id="value">{item}</h5>
                                     <div id="core-tools">
-                                        <div id="edit" onClick={(e) => {this.openEditModal(e, index)}}> <img src={editIco} style={{ maxWidth: "25px" }} /></div>
+                                        <div onClick={(e) => {this.openEditModal(e, index)}}> <img id ="edit" src={editIco} style={{ maxWidth: "25px" }} /></div>
                                         <div onClick={this.openShareModal}><img id="share" src={ShareIco} style={{ maxWidth: "25px" }} /></div>
                                         <div onClick={(e) => {this.openRemModal(e, index)}}><img id='close-remove-button' src={X} style={{ maxWidth: "35px" }} /></div>
                                     </div>
